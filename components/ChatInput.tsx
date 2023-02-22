c4093fa7d4a3f297121a2bb1d687cf526245623b
HyungJinHan <han1210_36@naver.com>
2023-02-23 00:03:35 +0900

    react-hot-toast 라이브러리를 통한 notification 작업 진행

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import { db } from "../firebase";
 
 type Props = {
   chatId: string;
@@ -12,9 +14,61 @@ const ChatInput = ({ chatId }: Props) => {
   const [prompt, setPrompt] = useState<string>("");
   const { data: session } = useSession();
 
+  // useSWR to get model
+  const model = "text-davinci-003";
+
+  const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!prompt) return;
+
+    const input = prompt.trim();
+    setPrompt("");
+
+    const message: Message = {
+      text: input,
+      createAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar:
+          session?.user?.image! ||
+          `https:ui-avatars.com/api/?name=${session?.user?.name}`,
+      },
+    };
+    // Toast Notification to say Loading...
+
+    await addDoc(
+      collection(
+        db,
+        "users",
+        session?.user?.email!,
+        "chats",
+        chatId,
+        "messages"
+      ),
+      message
+    );
+
+    // Toast Notification
+    await fetch("/api/askQuestion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: input,
+        chatId,
+        model,
+        session,
+      }),
+    }).then(() => {
+      // Toast Notification to say successful
+    });
+  };
+
   return (
     <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
-      <form className="p-5 space-x-5 flex">
+      <form onSubmit={sendMessage} className="p-5 space-x-5 flex">
         <input
           className="bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300"
           disabled={!session}
