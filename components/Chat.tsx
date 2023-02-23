6b17286f99be79357fa7ae3b21097f5227de93f6
HyungJinHan <han1210_36@naver.com>
2023-02-24 01:54:54 +0900

    ChatGPT 답변 얻어내기 성공
    기본적인 기능 및 스타일링 완성

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,9 +1,40 @@
+"use client";
+
+import { collection, orderBy, query } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+import Message from "./Message";
+
 type Props = {
   chatId: string;
 };
 
 const Chat = ({ chatId }: Props) => {
-  return <div className="flex-1">Chat</div>;
+  const { data: session } = useSession();
+
+  const [messages] = useCollection(
+    session &&
+      query(
+        collection(
+          db,
+          "users",
+          session?.user?.email!,
+          "chats",
+          chatId,
+          "messages"
+        ),
+        orderBy("createAt", "asc")
+      )
+  );
+
+  return (
+    <div className="flex-1">
+      {messages?.docs.map((message) => (
+        <Message key={message.id} message={message.data()} />
+      ))}
+    </div>
+  );
 };
 
 export default Chat;
--- pages/api/askQuestion.tsx
+++ pages/api/askQuestion.tsx
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import query from "../../lib/queryApi";
+import admin from "firebase-admin";
+import { adminDb } from "../../firebaseAdmin";
+import ChatImage from "../../public/ChatGPT_logo.svg";
 
 type Data = {
   answer: string;
@@ -25,9 +28,24 @@ export default async function handler(
   // ChatGPT Query
   const response = await query(prompt, chatId, model);
 
-  // const message: Message = {
-  //   text: response || "ChatGPT was unable to find an answer for that!",
-  // };
+  const message: Message = {
+    text: response || "ChatGPT was unable to find an answer for that!",
+    createAt: admin.firestore.Timestamp.now(),
+    user: {
+      _id: "ChatGPT",
+      name: "ChatGPT",
+      avatar:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1200px-ChatGPT_logo.svg.png",
+    },
+  };
 
-  res.status(200).json({ answer: "John Doe" });
+  await adminDb
+    .collection("users")
+    .doc(session?.user?.email)
+    .collection("chats")
+    .doc(chatId)
+    .collection("messages")
+    .add(message);
+
+  res.status(200).json({ answer: message.text });
 }
