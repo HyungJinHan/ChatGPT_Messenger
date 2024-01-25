0acce8b1791d9f6010dc5c20eabf318a9114a273
HyungJin Han <han1210_36@naver.com>
2024-01-25 16:34:44 +0900

    fix

--- lib/chatgpt.ts
+++ lib/chatgpt.ts
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-  basePath: "https://api.pawan.krd/v1",
+  basePath: "https://api.pawan.krd/v1/models",
 });
 
 const openai = new OpenAIApi(configuration);
