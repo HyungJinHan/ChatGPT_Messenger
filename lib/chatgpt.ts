50511ce41f492850e812c00598f05d267fee1e2c
HyungJin Han <han1210_36@naver.com>
2024-01-25 15:56:02 +0900

    fix: basePath fix

--- lib/chatgpt.ts
+++ lib/chatgpt.ts
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-  basePath: "https://api.pawan.krd/v1",
+  basePath: "https://api.pawan.krd/v1/chat/completions",
 });
 
 const openai = new OpenAIApi(configuration);
