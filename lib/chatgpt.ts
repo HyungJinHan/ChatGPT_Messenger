2358fcbc078cc37b539a9e324766cf0f2036282d
HyungJin Han <han1210_36@naver.com>
2024-01-25 15:27:24 +0900

    update: ChatGPT baseURL

--- lib/chatgpt.ts
+++ lib/chatgpt.ts
@@ -2,6 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  basePath: "https://api.pawan.krd/v1",
 });
 
 const openai = new OpenAIApi(configuration);
