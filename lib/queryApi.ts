09153bd1b78642903120da5248c5464078462243
HyungJin Han <han1210_36@naver.com>
2024-01-25 15:40:11 +0900

    fix: createCompletion code

--- lib/queryApi.ts
+++ lib/queryApi.ts
@@ -4,12 +4,13 @@ const query = async (prompt: string, chatId: string, model: string) => {
   const res = await openai
     .createCompletion({
       model,
-      prompt,
-      temperature: 0.9,
+      prompt: "Human: Hello\nAI:",
+      temperature: 0.7,
+      max_tokens: 256,
       top_p: 1,
-      max_tokens: 1000,
       frequency_penalty: 0,
       presence_penalty: 0,
+      stop: ["Human: ", "AI: "],
     })
     .then((res) => res.data.choices[0].text)
     .catch(
